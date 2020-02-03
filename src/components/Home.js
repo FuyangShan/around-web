8a66d1f5f4dbe9b77aa5f60e35455c9d881a0f88
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-02-03 12:34:07 -0800

    Add tabs in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import '../styles/Home.css';
+import { Tabs, Button } from 'antd';
 
+const { TabPane } = Tabs;
 export function Home() {
-    return (<div>Home</div>);
+    const operations = <Button>Create New Post</Button>;
+
+    return (
+        <Tabs tabBarExtraContent={operations} className="main-tabs">
+            <TabPane tab="Tab 1" key="1">
+                Content of tab 1
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+                Content of tab 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+                Content of tab 3
+            </TabPane>
+        </Tabs>
+    );
 };
+
